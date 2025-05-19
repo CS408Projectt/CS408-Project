6805b2c1afe149f760bbabeae658329da767f9ad
Eslin Gurcan <157117809+eslingurcan@users.noreply.github.com>
2025-05-19 16:10:27 +0300

    simulate crash for sensor

--- sensorFolder/sensor.py
+++ sensorFolder/sensor.py
@@ -4,6 +4,8 @@ import time
 import argparse
 import logging
 import random
+import sys
+import os
 from datetime import datetime, timezone
 
 logging.basicConfig(
@@ -50,13 +52,31 @@ def main():
     parser.add_argument("--interval", type=int, default=2, help="Interval between sensor data sends (seconds)")      # default is 2 secs
     parser.add_argument("--sensor_id", type=str, default="sensor1", help="Unique Sensor ID")                         # default is sensor1
     parser.add_argument("--reconnect_interval", type=int, default=5, help="Reconnect interval on failure (seconds)") # default is 5 secs
-
+    parser.add_argument("--simulate_crash_after", type=int, default=None, help="(Optional) Seconds before simulating crash")
+    parser.add_argument("--restart_delay", type=int, default=None, help="(Optional) Seconds to wait before restart")
+    
     args = parser.parse_args()
-
+    start_time = time.time()
     sock = connect_to_drone(args.drone_ip, args.drone_port, args.reconnect_interval)
 
+    
     while True:
         try: # connection is established
+            # simulate crash if parameters are set
+            if args.simulate_crash_after is not None and (time.time() - start_time) > args.simulate_crash_after:
+                logging.warning("A crash occurring now. Shutting down sensor...")
+                try:
+                    sock.close()
+                except Exception:
+                    pass
+                if args.restart_delay is not None:
+                    time.sleep(args.restart_delay)
+                    logging.info("Restarting sensor after crash.")
+                    os.execv(sys.executable, ['python'] + sys.argv)
+                else:
+                    logging.warning("Restart delay after crash is not specified. Exiting.")
+                    sys.exit(0)
+                
             data = generate_sensor_data(args.sensor_id)
             message = json.dumps(data)
             sock.sendall((message + '\n').encode('utf-8'))
@@ -69,6 +89,6 @@ def main():
             except Exception:
                 pass
             sock = connect_to_drone(args.drone_ip, args.drone_port, args.reconnect_interval)
-
+                
 if __name__ == "__main__":
     main()
